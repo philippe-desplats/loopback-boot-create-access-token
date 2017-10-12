69d1cae8852b94d1f1e10fb5993c64a23b5e70d3
Redek Project <redekproject@gmail.com>
2017-10-12 19:07:21 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# loopback-boot-create-access-token
+# Loopback Boot Script : Create access token
 Module for generating an access token with an administrator account with all accessess.
+
+## Installation
+```js
+npm install loppback-boot-create-access-token --save
+```
+
+## Usage
+
+## Tests
+```js
+npm test
+```
---
+++ test/test.js
@@ -0,0 +1,19 @@
+/**
+/* Script name : test.js
+/* Description : Script allowing to make a unit test in order to find possible errors and corrected them as quickly as possible.
+/* Author : Redek Project
+**/
+
+'use strict';
+
+// IMPORTS
+// ----------------------------------------------------------------------------
+	const expect = require('chai').expect;
+
+// TESTS
+// ----------------------------------------------------------------------------
+	describe('', () => {
+		it('', () => {
+
+		});
+	});
